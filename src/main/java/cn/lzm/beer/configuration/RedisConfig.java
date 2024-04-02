c8a8c5fc862ec6dcfc971b0e13ff64f75bf897dd
zemin.li <li.zemin@outlook.com>
2024-04-02 15:23:30 +0800

    引入redis

---
+++ src/main/java/cn/lzm/beer/configuration/RedisConfig.java
@@ -0,0 +1,23 @@
+package cn.lzm.beer.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        redisTemplate.setValueSerializer(RedisSerializer.java());
+        redisTemplate.setHashValueSerializer(RedisSerializer.java());
+        redisTemplate.setConnectionFactory(factory);
+        return redisTemplate;
+    }
+}
